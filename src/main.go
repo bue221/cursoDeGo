ef76e88efd16d43e1ef95736b5c05e2bac97718d
bue221 <camiloplaza3@gmail.com>
2021-03-25 21:54:20 -0500

    operadores

--- src/main.go
+++ src/main.go
@@ -3,33 +3,54 @@ package main
 
 import "fmt"
 
-
 func main() {
-	// costantes
-	const pi float64 = 3.14
-	const pi2 =3.14
-
-	fmt.Println("pi ",pi)
-	fmt.Println("pi2: ",pi2)
-
-	//variables
-	base := 12
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	//Zero values
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a,b,c,d)
 
 	// area de un cuadrado
-	const  baseCudrado = 10
+	const baseCudrado = 10
 	areaCuadrado := baseCudrado * baseCudrado
 
-	fmt.Println("El area del cuadrado es: ",areaCuadrado)
+	fmt.Println("El area del cuadrado es: ", areaCuadrado)
+
+	//variables
+	x := 10
+	y := 39
+
+	//suma
+	suma := x + y
+	fmt.Println("suma: ", suma)
+	//resta
+	resta := x - y
+	fmt.Println("resta: ", resta)
+	//multiplicacion
+	multi := x * y
+	fmt.Println("Multiplicacion: ", multi)
+	//division
+	div := y / x
+	fmt.Println("Division: ", div)
+	//modulo
+	mod := y % x
+	fmt.Println("Modulo: ", mod)
+	// Incremental el dcremental seria con --
+	x++
+	fmt.Println("Incremental: ", x)
+
+	//area de un rectangulo
+	const base = 40
+	const altura = 30
+	areaRect := base * altura
+	fmt.Println("Area rectangulo: ", areaRect)
+
+	//area de un circulo
+	const radio = 4
+	const pi = 3.14
+	areaCir := (pi) * (radio * radio)
+	fmt.Println("Area rectangulo: ", areaCir)
+
+	//area de un trapecio
+	baseUno := 6
+	baseDos := 15
+	alturaTra := 25
+
+	areaTrap := ((baseUno + baseDos) * alturaTra) / 2
+	fmt.Println("Area trapecio: ", areaTrap)
 }
