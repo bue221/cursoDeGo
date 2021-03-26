722a9b613c48e156b9f618f6c6a66213830ca683
bue221 <camiloplaza3@gmail.com>
2021-03-25 22:16:00 -0500

    funciones en go

--- src/main.go
+++ src/main.go
@@ -3,10 +3,32 @@ package main
 
 import "fmt"
 
+func normalFunction(message string) {
+	fmt.Println(message)
+}
+
+func tripeArgument(a int, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a + 1
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
 func main() {
 
-	helloMessage := "hello"
-	worldMessage := "world"
+	normalFunction("Buena perro")
+	tripeArgument(1, 2, "Loca")
+	fmt.Println(returnValue(34))
+	value1, value2 := doubleReturn(2)
+	fmt.Println("Values: ", value1, value2)
+	// // se repite
+	// fmt.Println("Hola mundo")
+	// fmt.Println("Hola mundo 2")
+	// fmt.Println("Hola mundo 3")
 
-	fmt.Println(helloMessage, worldMessage)
 }
