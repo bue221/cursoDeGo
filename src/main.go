189e5ec661a3e602d435671814e1d9c085e41856
bue221 <camiloplaza3@gmail.com>
2021-03-25 21:36:36 -0500

    variables y constantes en go

--- src/main.go
+++ src/main.go
@@ -3,6 +3,33 @@ package main
 
 import "fmt"
 
+
 func main() {
-	fmt.Println("Hola mundo")
+	// costantes
+	const pi float64 = 3.14
+	const pi2 =3.14
+
+	fmt.Println("pi ",pi)
+	fmt.Println("pi2: ",pi2)
+
+	//variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a,b,c,d)
+
+	// area de un cuadrado
+	const  baseCudrado = 10
+	areaCuadrado := baseCudrado * baseCudrado
+
+	fmt.Println("El area del cuadrado es: ",areaCuadrado)
 }
